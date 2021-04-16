ddc144c6696a8f11f0e955b65de6228a51282158
Danielalab <olabolacrayola@hotmail.com>
2021-04-16 13:31:02 -0500

    feat(storage): add diskStorage config

---
+++ src/storage/disk.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    /* Note: You are responsible for creating
+    the directory when providing destination as
+    a function. When passing a string, multer
+    will make sure that the directory is created
+    for you. */
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+    /* Note: Multer will not append any file
+    extension for you, your function should
+    return a filename complete with an file
+    extension. */
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage });
+
+module.exports = upload;
