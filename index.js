f406bffb32ffb765602f81d092a44bcce703650f
Danielalab <olabolacrayola@hotmail.com>
2021-04-24 17:01:26 -0500

    feat(index.js): add CORS config

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./src/routes/index');
+const { client } = require('./config');
 const port = 8080;
 
 /* Note: I can't use app.use(express.json())
@@ -14,7 +16,7 @@ Here it's executed every time that the app receives a request.
 
 /* Express.static: it's a middleware that serve  to serve static
 files like CSS files, images, Js files*/
-
+app.use(cors({ origin: client }));
 app.use('/uploads', express.static('uploads'));
 
 app.listen(port, () => {
